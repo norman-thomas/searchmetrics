fda643803a2beba3fd3faa4f2f845f23ac221b37
Norman Thomas <news@hiroki.de>
2018-10-12 08:23:57 +0200

    preparing release

--- mix.exs
+++ mix.exs
@@ -37,13 +37,12 @@ defmodule SearchMetrics.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:distillery, "~> 2.0"},
       {:wallaby, "~> 0.20.0", [runtime: true]},
       {:floki, "~> 0.20.4"},
-      {:excoveralls, "~> 0.10", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
       {:elixir_google_spreadsheets, "~> 0.1.9"}
     ]
   end
